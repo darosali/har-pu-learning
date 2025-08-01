1d2285d9bc547c7c78fd1f7ee2176d9bb778c12d
Alina_Daroshka <bebra@DESKTOP-A459N8O.localdomain>
2025-08-01 06:18:28 +0200

    Add uPU and nnPU losses

--- baseline.py
+++ baseline.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
+from pu_loss import PULoss
 
 class Conv_Net(nn.Module):
     def __init__(self, in_channel, seq_length, hidden_dim, n_output):
@@ -84,15 +85,25 @@ def train(model, criterion, train_loader, test_loader, lr, epochs, device):
             optimizer.zero_grad()
             
             outputs = model(inputs)
-            loss = criterion(outputs, labels)
+
+            if isinstance(criterion, PULoss):
+                loss = criterion(outputs.view(-1), labels)
+                preds = torch.where(
+                    outputs > 0,
+                    torch.tensor(1, device=outputs.device),
+                    torch.tensor(-1, device=outputs.device)
+                ).view(-1) 
+            else:
+                loss = criterion(outputs, labels)
+                preds = torch.max(outputs, 1)[1]
+           
+            
             loss.backward()
-            optimizer.step()
-            
-            preds = torch.max(outputs, 1)[1]
-            
+            optimizer.step() 
+                   
             train_loss += loss.item() * train_batch_size 
             n_train_acc += (preds == labels).sum().item()
-            
+        
         model.eval()
         
         for inputs_test, labels_test in test_loader:
@@ -105,12 +116,21 @@ def train(model, criterion, train_loader, test_loader, lr, epochs, device):
             labels_test = labels_test.to(device)
             
             outputs_test = model(inputs_test)
-            loss_test = criterion(outputs_test, labels_test)
-            preds_test = torch.max(outputs_test, 1)[1]
             
-            test_loss +=  loss_test.item() * test_batch_size
-            n_test_acc +=  (preds_test == labels_test).sum().item()
+            if isinstance(criterion, PULoss):
+                loss_test = criterion(outputs_test.view(-1), labels_test)
+                preds_test = torch.where(
+                    outputs_test > 0,
+                    torch.tensor(1, device=outputs_test.device),
+                    torch.tensor(-1, device=outputs_test.device)
+                ).view(-1)
+            else:
+                loss_test = criterion(outputs_test, labels_test)
+                preds_test = torch.max(outputs_test, 1)[1]
             
+            test_loss += loss_test.item() * test_batch_size
+            n_test_acc += (preds_test == labels_test).sum().item()
+        
         train_acc = n_train_acc / n_train
         test_acc = n_test_acc / n_test
         avg_train_loss = train_loss / n_train
